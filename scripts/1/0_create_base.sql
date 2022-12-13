8bd8720db3a4a22fa473ce31a1491234c4f0da56
Lev Nemirovskii <levnemir@yandex.ru>
2022-12-13 16:45:05 +0300

    Скрипт для master slave репликации

---
+++ scripts/1/0_create_base.sql
@@ -0,0 +1,31 @@
+
+  -- Create base table
+
+  -- BOOKS table
+  --	 	book_id	  - ID of book (primary key)
+  --		title 	  - title of book
+  --		category  - book's category
+  --		author	  - book's author
+  CREATE TABLE t_books (
+      book_id 		INT 			      NOT NULL,
+      title 			VARCHAR(100) 	NOT NULL,
+      category		VARCHAR(30),
+      author 			VARCHAR(100)   NOT NULL,
+      is_active   VARCHAR(1)     NOT NULL,
+      CONSTRAINT t_books_id_pk PRIMARY KEY (book_id)
+  );
+
+  -- insert base data
+  UPDATE t_books
+    SET title = 'Oracle Core', category = 'Oracle Database', author = 'Jonathan Lewis', is_active = 'Y'
+    WHERE book_id = 3001;
+
+  UPDATE t_books
+    SET title = 'Expert Oracle Database Architecture', category = 'Oracle Database', author = 'Tom Kyte', is_active = 'Y'
+    WHERE book_id = 2025;
+
+  UPDATE t_books
+    SET title = 'SQL and Relational Theory', category = 'Relational Databases', author = 'C.J. Date', is_active = 'Y'
+    WHERE book_id = 190;
+
+  COMMIT;
